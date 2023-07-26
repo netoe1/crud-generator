ed1f9762ca78ef24672468fdba39c19b7b4be7a0
netoe1 <elynetobr@outlook.com>
2023-07-26 10:30:40 -0300

    adding a cli

--- src/interface.h
+++ src/interface.h
@@ -3,21 +3,20 @@
 #include <string.h>
 int cli(int *argc, char *argv[])
 {
-    char *str = { "
-                  Usage : crudgen<fn><args>\
-
-                  "};
+    char *str = "\\
+                Usage: program <function> <args> \\
+                \nFunction:                        \\
+                \ncreate_crud     generates a default crud\\
+                \nexit            close the program.    \\"                                                 
+                                                    ;
 
                   setlocale(LC_ALL, "");
 
     if (strcmp(argv[1], "--help") || strcmp(argv[1], "-h"))
     {
-        puts(
-            "\\
-            \ncrudgen<function><arguments> "
-
-        );
+        puts(str);
     }
+    
 
     return 1;
 }
